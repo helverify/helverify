e27e7f184519138abd6b8f18eb494e3445fa7a47
Niels Kübler <niels.kuebler@uzh.ch>
2022-07-27 18:03:46 +0200

    Implemented manual verification step for voter frontend

--- voter/frontend/src/components/validation/ManualValidationStep.tsx
+++ voter/frontend/src/components/validation/ManualValidationStep.tsx
@@ -1,10 +1,9 @@
 import {ValidationStepProps} from "./validationStep";
-import {Button, Typography} from "@mui/material";
+import {Box, Button, Grid, Stack, Typography} from "@mui/material";
 import {CastBallotView} from "../CastBallotView";
 import {CastBallot} from "../../ballot/castBallot";
 import {SpoiltBallot} from "../../ballot/spoiltBallot";
 import {SpoiltBallotView} from "../SpoiltBallotView";
-
 
 export type ManualValidationStepProps = ValidationStepProps & {
     castBallot: CastBallot | undefined;
@@ -12,17 +11,33 @@ export type ManualValidationStepProps = ValidationStepProps & {
 }
 
 export const ManualValidationStep = (props: ManualValidationStepProps) => {
-    return(
+    return (
         <>
-            <Typography>Manual Validation</Typography>
+            <Typography variant={"h5"}>Manual Validation</Typography>
+            <Typography>This verification step allows you to (a) verify that your selections have been registered
+                correctly and (b) verify that the unselected column of your ballot matches with the short codes shown
+                here.</Typography>
+            <Grid container>
                 {props.castBallot !== undefined && (
-                    <CastBallotView ballot={props.castBallot}/>
+                    <Grid item xs={12} sm={6}>
+                        <CastBallotView ballot={props.castBallot}/>
+                    </Grid>
                 )}
                 {props.spoiltBallot !== undefined && props.electionParameters !== undefined && props.ballots !== undefined && (
-                    <SpoiltBallotView ballot={props.spoiltBallot} electionParameters={props.electionParameters} encryptions={props.ballots}/>
+                    <Grid item xs={12} sm={6}>
+                        <SpoiltBallotView ballot={props.spoiltBallot} electionParameters={props.electionParameters}
+                                          encryptions={props.ballots}/>
+                    </Grid>
                 )}
-            <Button onClick={props.previous}>Previous</Button>
-            <Button onClick={props.next}>Next</Button>
+            </Grid>
+            <Stack direction="row" spacing={1} style={{marginTop: "10px"}}>
+                <Box display="flex" justifyContent="left" alignItems="left" flexGrow={1}>
+                    <Button onClick={props.previous} variant="outlined">Previous</Button>
+                </Box>
+                <Box display="flex" justifyContent="right" alignItems="right">
+                    <Button onClick={props.next} variant="contained">Next</Button>
+                </Box>
+            </Stack>
         </>
     );
 }
